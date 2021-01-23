9c2bb8ec2c3391bf80064bf44d96a7aba0f487d8
Micah Pearce <micah.r.pearce@gmail.com>
2021-01-23 15:09:44 -0600

    add note for when working

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(cors())
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get('/', (req, res)=> {})
+app.get('/', (req, res)=> {res.send('it is working!'}))
 // this (making sure to change the controller file in the appropriate way)
 // handleImage(db)
 app.post('/signin', handleSignin(db, bcrypt))
