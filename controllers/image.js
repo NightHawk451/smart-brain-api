7fee20f70062c269e91bf6d3542db5cac4228ae1
Micah Pearce <micah.r.pearce@gmail.com>
2021-01-23 16:28:30 -0600

    move key to heroku

--- controllers/image.js
+++ controllers/image.js
@@ -2,7 +2,7 @@
 import Clarifai from 'clarifai'
 
 const app = new Clarifai.App({
-  apiKey: ProcessingInstruction.env.API_CLARIFAI
+  apiKey: process.env.API_CLARIFAI
 })
 
 export const handleApiCall = (req, res) => {
