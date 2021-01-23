db4d0c8fb8f8e11dc87f70bf6053b25812a9b95f
Micah Pearce <micah.r.pearce@gmail.com>
2021-01-23 15:52:34 -0600

    update image

--- controllers/image.js
+++ controllers/image.js
@@ -6,23 +6,32 @@ const app = new Clarifai.App({
 })
 
 export const handleApiCall = (req, res) => {
-  console.log('we are on the image api call')
   app.models
-    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
-    .then(data => {
-      res.json(data)
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input) // get body input
+    .then(data => { 
+      res.json(data);
     })
-    .catch(err => res.status(400).json('unable to work with API'))
-}
+    .catch(err => res.status(400).json('unable to work with API')) // catch error
+ }
 
-export const handleImage = (req, res, db) => {
-  const { id } = req.body;
-  db('users').where('id', '=', id)
-    .increment('entries', 1)
-    .returning('entries')
-    .then(entries => {
-      res.json(entries[0])
+    // HEADS UP! Sometimes the Clarifai Models can be down or not working as they are constantly getting updated.
+   // A good way to check if the model you are using is up, is to check them on the clarifai website. For example,
+   // for the Face Detect Mode: https://www.clarifai.com/models/face-detection
+   // If that isn't working, then that means you will have to wait until their servers are back up. Another solution
+   // is to use a different version of their model that works like: `c0c0ac362b03416da06ab3fa36fb58e3`
+   // so you would change from:
+   // .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+   // to:
+   // .predict('c0c0ac362b03416da06ab3fa36fb58e3', this.state.input)
+   
+
+export const handleImage = (req, res, db) => { // dependency injection, passing 'db' to image.js from server.js
+    const { id } = req.body; // capture id
+    db('users').where('id', '=', id) // connect to db where id matches
+    .increment('entries', 1) // increment entry value by +1
+    .returning('entries') // return new entry value 
+    .then(entries => { // 
+        res.json(entries[0]); // reply with update entry value for user
     })
-    .catch(err => res.status(400).json('unable to increment'))
- 
+    .catch(err => res.status(400).json('unable to get entries'))
 }
