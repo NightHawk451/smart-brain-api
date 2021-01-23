3169f6d75e8bdb2dca2ec834917beae0566242ec
Micah Pearce <micah.r.pearce@gmail.com>
2021-01-23 15:57:02 -0600

    update image

--- controllers/image.js
+++ controllers/image.js
@@ -11,7 +11,7 @@ export const handleApiCall = (req, res) => {
     .then(data => { 
       res.json(data);
     })
-    .catch(err => res.status(400).json('unable to work with API')) // catch error
+    .catch(err => res.status(400).json(err)) // catch error
  }
 
     // HEADS UP! Sometimes the Clarifai Models can be down or not working as they are constantly getting updated.
